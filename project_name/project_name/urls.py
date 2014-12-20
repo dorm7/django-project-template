4a2dbafaa97d6a1c98a48909610e1a6c762d12c2
tim <tim@timdeMacBook-Pro.local>
2014-12-20 10:41:11 +0800

    add templates

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^_base/', TemplateView.as_view(template_name='base.html')),
 )
